bdaca0fbf14fb09b68bcd0fbe63b661c2a94dae1
Priyanshu Arora <54005333+PriyanshuArora2001@users.noreply.github.com>
2022-01-23 18:46:04 +0530

    Update Contact.java

--- app/src/main/java/com/example/aahaarapp/Contact.java
+++ app/src/main/java/com/example/aahaarapp/Contact.java
@@ -1,9 +1,11 @@
 package com.example.aahaarapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -11,12 +13,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Contact extends AppCompatActivity {
     EditText name, email, message;
     Button submit;
     boolean isNameValid, isEmailValid, isMessageValid;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    public static final String TAG = "TAG";
     TextInputLayout nameError, emailError, messageError;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +46,10 @@ public class Contact extends AppCompatActivity {
         nameError = (TextInputLayout) findViewById(R.id.nameError);
         emailError = (TextInputLayout) findViewById(R.id.emailError);
         messageError = (TextInputLayout) findViewById(R.id.messageError);
+
+        fAuth=FirebaseAuth.getInstance();
+        fStore= FirebaseFirestore.getInstance();
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,6 +58,7 @@ public class Contact extends AppCompatActivity {
         });
     }
         public void SetValidation() {
+
             // Check for a valid name.
             if (name.getText().toString().isEmpty()) {
                 nameError.setError(getResources().getString(R.string.name_error));
@@ -68,9 +90,40 @@ public class Contact extends AppCompatActivity {
             }
 
             if (isNameValid && isEmailValid && isMessageValid ) {
-                Toast.makeText(getApplicationContext(), "Successfully! We will shortly revert you back.", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+
+                String Name = name.getText().toString().trim();
+                String Email= email.getText().toString().trim();
+                String Message= message.getText().toString().trim();
+                userID = fAuth.getCurrentUser().getUid();
+                //DocumentReference documentReference = fStore.collection("donate").document(userID);
+                CollectionReference collectionReference = fStore.collection("contact data");
+
+                Map<String,Object> user = new HashMap<>();
+                user.put("timestamp", FieldValue.serverTimestamp());
+                user.put("name",Name);
+                user.put("email",Email);
+                user.put("message",Message);
+                user.put("userid",userID);
+
+                collectionReference.add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getApplicationContext(),"Success!",Toast.LENGTH_SHORT).show();
+                                Log.d(TAG,"Successfully! We will shortly revert you back.");
+                                //startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                Intent intent = new Intent(Contact.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(),"Error!",Toast.LENGTH_SHORT).show();
+                                Log.w(TAG, "Error!", e);
+                            }
+                        });
             }
 
         }
